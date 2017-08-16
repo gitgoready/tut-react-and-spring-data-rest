734ce14f2bfd87d7d40da82276d18eb5eb553eb6
goready <goready@163.com>
2017-08-16 15:41:11 +0800

    1、将spring data rest纳入token保护范围，但不能在swagger输入token，repository方法暂时不能同时支持dao映射及映射http header，只能用postman测试 2、UserDetailService使用的findUserBy方法不能保护，因为被权限系统使用，否则权限系统无法工作

--- security/src/main/java/cn/connectai/myai/entity/UserRepository.java
+++ security/src/main/java/cn/connectai/myai/entity/UserRepository.java
@@ -18,7 +18,9 @@ package cn.connectai.myai.entity;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -29,6 +31,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 //@PreAuthorize("hasAnyRole('ROLE_MANAGER','ROLE_USER')")
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 
+	@PostAuthorize("returnObject.sso == principal.username or hasRole('ROLE_MANAGER')")
+	User findByFirstName(@Param("firstName") @RequestParam(value="firstName") String firstName);//@RequestHeader(value="Authorization") String token
+
+	//此处为权限系统JwtUserDetailsServiceImpl UserDetails loadUserByUsername调用,不能再加权限控制,否则出错,最好ignore
 	User findBySso(@Param("sso") @RequestParam(value="sso") String sso);
 
 	@Override
@@ -36,7 +42,7 @@ public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 	User save(@Param("user") User user);
 
 	@Override
-	@PreAuthorize("@userRepository.findOne(#id)?.manager?.name == authentication?.name")
+	@PreAuthorize("@userRepository.findOne(#id)?.manager?.name == authentication?.name")//无经理,或所删用户的经理 正是 当前经过认证的经理,使用ManagerUserDetailService
 	void delete(@Param("id") Long id);
 
 	@Override
